4389ee8070a7fa7b51a12ec523cbce3b0298ce08
Giovanni Ortuño Urquidi <gd.ortuno@gmail.com>
2015-10-01 14:15:54 -0700

    Boilerplate app

---
+++ background.js
@@ -0,0 +1,14 @@
+/**
+ * Listens for the app launching, then creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  chrome.app.window.create(
+    'index.html',
+    {
+      state: 'fullscreen'
+    }
+  );
+});
